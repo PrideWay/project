03fb881e57f83c34ca74592196a93d3ed7625f5c
PrideWay <zakharserkov@me.com>
2018-07-05 16:36:56 +0300

    Соорудил шапку

--- footer.php
+++ footer.php
@@ -1,1 +1,18 @@
-<?php
+</div>
+
+<?php get_template('sidebar'); ?>
+
+</div>
+
+<div class="footer">
+    <div class="footer__copy">All rights reserved. &copy; 2018</div>
+    <div class="footer__creator">
+        made by Zakhar
+    </div>
+</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,22 @@
-<?php
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-4 socials">.col-md-4</div>
+        <div class="col-md-4 logo">.col-md-4</div>
+        <div class="col-md-4 phone">
+            <p>Кератиновое выпрямление волос</p>
+            <a class="aphone" href="tel:+79661778883"><span>+7 (966)</span> 177-888-3 </a>
+        </div>
+    </div>
